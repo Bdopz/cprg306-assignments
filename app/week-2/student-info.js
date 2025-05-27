3f6213d56ac34415a236046a6fc39145971eaebe
Bdopz <bdopz0883@gmail.com>
2025-05-27 10:15:04 -0600

    Week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export function StudentInfo() {
+  return (
+    <div>
+
+    <p>Theodore Frocklage</p>
+    <Link href="https://github.com/Bdopz">GitHub Profile</Link>
+
+    </div>
+  );
+}
