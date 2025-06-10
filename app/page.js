7f2670ed769edf92a34752adce99a819c6aef42d
Bdopz <bdopz0883@gmail.com>
2025-06-09 20:28:52 -0600

    Week 3

--- app/page.js
+++ app/page.js
@@ -5,7 +5,14 @@ export default function Home() {
     <main>
 
     <h1 className="m-2">CPRG 306: Web Development 2 - Assignments</h1>
-    <Link href="/week-2">Week 2</Link>
+    <ul className="m-2">
+      <li>
+        <Link href="/week-2">Week 2</Link>
+      </li>
+      <li>
+        <Link href="/week-3">Week 3</Link>
+      </li>
+    </ul>
 
     </main>
   );
